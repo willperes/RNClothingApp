7329c538dc7ea7f8c260e03acdb8cd4388dadd3f
Willian <thewidocs@gmail.com>
2026-03-07 16:11:58 -0300

    feat: created HttpClient abstraction and axios implementation

---
+++ src/infra/adapters/axios.adapter.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+import { HttpError } from "../http";
+import { HttpClient, HttpRequest, HttpResponse } from "../http/http-client";
+
+export class AxiosHttpClientAdapter implements HttpClient {
+  private _formatResponse(response: AxiosResponse): HttpResponse {
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  }
+
+  private _formatError(error: AxiosError): HttpError {
+    const statusCode = error.response?.status ?? 500;
+    return new HttpError(statusCode, error.response?.data);
+  }
+
+  async request(request: HttpRequest): Promise<HttpResponse> {
+    return axios
+      .request({
+        url: request.url,
+        method: request.method,
+        data: request.body,
+        headers: request.headers,
+        auth: request.auth,
+      })
+      .then(this._formatResponse)
+      .catch((error: AxiosError) => {
+        throw this._formatError(error);
+      });
+  }
+}
---
+++ src/infra/http/http-client.ts
@@ -0,0 +1,35 @@
+export type HttpAuth = {
+  username: string;
+  password: string;
+};
+
+export type HttpMethod = "get" | "post" | "put" | "delete";
+
+export type HttpRequest = {
+  url: string;
+  method: HttpMethod;
+  body?: any;
+  headers?: any;
+  auth?: HttpAuth;
+};
+
+export type HttpResponse<T = any> = {
+  statusCode: number;
+  data: T;
+};
+
+export class HttpError {
+  statusCode: number;
+  data: any;
+  name: string;
+
+  constructor(statusCode: number, body: any) {
+    this.statusCode = statusCode;
+    this.data = body;
+    this.name = "HttpError";
+  }
+}
+
+export interface HttpClient {
+  request: <R = any>(request: HttpRequest) => Promise<HttpResponse<R>>;
+}
